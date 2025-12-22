53c2493f17dbec717af74ea1906aa0f264fc02fb
Muetzilla <muetz-github@gasbichler.ch>
2025-12-22 18:46:29 +0100

    Anzahl Operationen

--- Verschluesselungsverfahren/src/main/java/ch/maturaarbeit/ciphers/hill/Hill.java
+++ Verschluesselungsverfahren/src/main/java/ch/maturaarbeit/ciphers/hill/Hill.java
@@ -56,37 +56,45 @@ public class Hill implements Cipher<HillParams, HillParams> {
             int charsToAdd = blockSize - (plaintext.length() % blockSize);
             for (int i = 0; i < charsToAdd; i++) {
                 results[results.length - 1] += "X";
+                // Möglicherweise hier operationsCount++
+                // operationsCount++;
             }
         }
-        //TODO think if this increases operations count
-        for (String result : results) {
 
+        for (String result : results) {
             SimpleMatrix tempVector = new SimpleMatrix(blockSize, 1);
 
             for (int i = 0; i < result.length(); i++) {
                 tempVector.set(i, (int) result.charAt(i) - 65);
+                // Anzahl der Operationen wird erhöht, da ein Schritt nötig ist, um das Zeichen in eine Zahl umzuwandeln.
+                operationsCount++;
             }
             plaintextBlocks.add(tempVector);
 
         }
 
         int keyCharCounter = 0;
-        //TODO: if there are not enough chars ad "X" at the end
         for (int i = 0; i < blockSize; i++) {
             for (int j = 0; j < blockSize; j++) {
                 keyMatrix.set(i, j, (int) key.charAt(keyCharCounter) - 65);
                 keyCharCounter++;
+                // Anzahl der Operationen wird erhöht, da die Schlüsselmatrik Schritt für Schritt aufgebaut und befüllt werden muss.
+                operationsCount++;
             }
         }
+
         ArrayList<SimpleMatrix> multipliedVectors = new ArrayList<>();
         for (SimpleMatrix vector : plaintextBlocks) {
             multipliedVectors.add(keyMatrix.mult(vector));
+            // Anzahl der Operationen wird erhöht, da die Matrixmultiplikation durchgeführt wird.
             operationsCount++;
         }
+
         for (SimpleMatrix v : multipliedVectors) {
             for (int i = 0; i < v.numRows(); i++) {
                 double value = v.get(i, 0);
                 cipherText.append((char) ((int) (value % 26) + 65));
+                // Anzahl der Operationen wird erhöht, da das Ergebnis in ein Zeichen umgewandelt werden muss.
                 operationsCount++;
             }
         }
